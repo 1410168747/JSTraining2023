30beaeaf4b79b7b30ba121d7e67727145883018a
zDpxq6 <tsuguka@gmail.com>
2024-05-11 16:14:11 +0900

    removed inappropriate comments

--- ch11/ex03/index.test.js
+++ ch11/ex03/index.test.js
@@ -19,28 +19,3 @@ describe("byteSapping", () => {
     expect(big2Little(target)).toStrictEqual(Uint32Array.of(0x67452301));
   });
 });
-
-// describe("cachedSlowFn", () => {
-
-//   test("It doesn't take long to get the same value from cachedSlowFn", async () => {
-//     const cachedSlowFn = cache(slowFn);
-//     await cachedSlowFn({})
-//     const expected = {};
-//     const startTime = performance.now();
-//     const actual = await cachedSlowFn(expected)
-//     const time = performance.now() - startTime;
-//     expect(actual).toBe(expected);
-//     expect(time).toBeLessThan(3000);
-//   }, 12000);
-
-//   test("Getting values from cachedSlowFn for the first time takes time.", async () => {
-//     const cachedSlowFn = cache(slowFn);
-//     await cachedSlowFn({})
-//     const expected = { new: "new" };
-//     const startTime = performance.now();
-//     const actual = await cachedSlowFn(expected)
-//     const time = performance.now() - startTime;
-//     expect(actual).toBe(expected);
-//     expect(time).toBeGreaterThanOrEqual(2000);
-//   }, 12000);
-// });
