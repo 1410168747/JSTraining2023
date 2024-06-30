22fcdd4553d200b80a93da225d5d886ab102c93b
zDpxq6 <tsuguka@gmail.com>
2024-07-01 02:33:47 +0900

    added

--- ch14/ex02/index.js
+++ ch14/ex02/index.js
@@ -1,11 +1,24 @@
 export class MyArrayLike {
-  // TODO
+
+  constructor(length) {
+    this.length = length;
+  }
+
+  *[Symbol.iterator]() {
+    for (let i = 0; i < this.length; i++) {
+      yield this[i];
+    }
+  };
+
 }
 
 export class MyArray extends Array {
+
+  static get [Symbol.species]() {
+    return MyArrayLike;
+  }
+
   constructor(items) {
     super(...items);
   }
-
-  // TODO
 }
