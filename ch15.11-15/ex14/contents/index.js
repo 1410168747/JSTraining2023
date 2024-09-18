c733ad3407ab701dbb3ede1987767f4c53dae25e
zDpxq6 <tsuguka@gmail.com>
2024-09-19 04:02:46 +0900

    added

--- ch15.11-15/ex14/contents/index.js
+++ ch15.11-15/ex14/contents/index.js
@@ -12,5 +12,24 @@ async function getMessageFromServer() {
   messageElement.textContent = "";
   messageContainer.appendChild(messageElement);
 
-  // TODO: ここにサーバーとのやり取り等を実装しなさい
+  button.disabled = true;
+
+  const eventSource = new EventSource("http://localhost:3000/message");
+
+  eventSource.onmessage = function (event) {
+    const messageData = JSON.parse(event.data);
+
+    messageElement.textContent += messageData.value;
+
+    if (messageData.done) {
+      eventSource.close();
+      button.disabled = false;
+    }
+  };
+
+  eventSource.onerror = function () {
+    messageElement.textContent += "Error receiving message from server";
+    eventSource.close();
+    button.disabled = false;
+  };
 }
