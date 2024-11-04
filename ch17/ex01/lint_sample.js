9a0e614ec6100d13b8408f49d484faf30cf9c63f
zDpxq6 <tsuguka@gmail.com>
2024-11-04 19:54:34 +0900

    added

--- ch17/ex01/lint_sample.js
+++ ch17/ex01/lint_sample.js
@@ -1,10 +1,8 @@
-let a, x, y;
 const r = 10;
 
-with (Math){
-  a = PI  * r * r;
-  x = r * cos(PI);
-  y = r * sin(PI / 2);
-}
+// with文を削除
+const a = Math.PI * r * r;
+const x = r * Math.cos(PI);
+const y = r * Math.sin(PI / 2);
 
 console.log(a, x, y);
