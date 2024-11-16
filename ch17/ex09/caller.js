1ace431b06b97b24dee30a2813b0dc66bc1a01b8
zDpxq6 <tsuguka@gmail.com>
2024-11-16 23:28:04 +0900

    updated

--- ch17/ex09/caller.js
+++ ch17/ex09/caller.js
@@ -1,40 +1,33 @@
-import { TaskManager, isLowOrCompletedTask, not } from "./task.flow.js";
-
+import { TaskManager, isLowOrCompletedTask, not } from "./task";
 const user1 = { id: 1, name: "Alice" };
 const user2 = { id: 2, name: "Bob" };
-
 const taskManager = new TaskManager();
-
 taskManager.add({
-  title: "テキストを読む",
-  completed: false,
-  user: user1,
-  priority: "high",
+    title: "テキストを読む",
+    completed: false,
+    user: user1,
+    priority: "high",
 });
-
 taskManager.add({
-  title: "質問表を書く",
-  completed: false,
-  user: user1,
-  priority: "middle",
+    title: "質問表を書く",
+    completed: false,
+    user: user1,
+    priority: "middle",
 });
-
 taskManager.add({
-  title: "質問表を確認する",
-  completed: false,
-  user: user2,
-  priority: "low",
+    title: "質問表を確認する",
+    completed: false,
+    user: user2,
+    priority: "low",
 });
-
 taskManager.add({
-  title: "問題を作成する",
-  completed: false,
-  user: user2,
-  priority: "middle",
+    title: "問題を作成する",
+    completed: false,
+    user: user2,
+    priority: "middle",
 });
-
 taskManager.completeTask(user1);
 taskManager.completeTask("質問表を確認する");
-
+console.log("this is the result of ch17/ex09/caller.ts");
 console.log(taskManager.getTasks());
 console.log(taskManager.getTasks(not(isLowOrCompletedTask)));
